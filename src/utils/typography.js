ae007c1d55dc459d11a1e1d596a5f078611a9395
sbahamon <sbahamon1@gmail.com>
2020-04-24 23:14:22 -0500

    changed icon

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -11,7 +11,6 @@ import fairyGatesTheme from 'typography-theme-fairy-gates'
 
 const typography = new Typography(fairyGatesTheme)
 
-
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
   typography.injectStyles()
